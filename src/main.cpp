243d4ae50c083ab4a697494ad99fb361a9f9c32a
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2025-07-18 22:30:08 -0600

    create discord ipc client socket class

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,20 @@
 #include <format>
 #include <string>
 
-#include "include/socket_client.hpp"
+#include "include/discord_ipc_client.hpp"
 #include "include/json.hpp"
 #include "include/parser.hpp"
-#include "include/utils.hpp"
 
-using websockets::SocketClient;
-using utils::find_discord_ipc_file;
+using discordipc::DiscordIPCClient;
 using json::JSON;
 using json::JSONObject;
 using json::Parser;
 
 int main() {
-  std::string discord_ipc_file = find_discord_ipc_file();
   // std::string music_client_id = "773825528921849856";  // apple music
   std::string music_client_id = "1313374141960949831";  // spwifiy
 
-  JSONObject handshake_json = {
-    {"v", JSON(1)},
-    {"client_id", JSON(music_client_id)}
-  };
-  std::string handshake_data = JSON(handshake_json).to_string();
-
-  std::cout << handshake_data << std::endl;
-
-  SocketClient client(discord_ipc_file);
+  DiscordIPCClient client(music_client_id);
 
   bool ret = client.connect();
 
@@ -43,19 +32,7 @@ int main() {
     return 1;
   }
 
-  ret = client.send_data(0, handshake_data);
-
-  if (!ret) {
-    std::cout << "failed to send packet data" << std::endl;
-
-    return 1;
-  }
-
-  std::string data = client.recv_data();
-  JSON recv_json = Parser::parse(data);
-
-  std::cout << data << std::endl;
-  std::cout << recv_json.to_string() << std::endl;
+  system("read");
 
   client.close();
 
